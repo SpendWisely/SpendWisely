d7581fb14933abfb7fcd0d51ea849108cf68b385
Pratishtha Kumari <pratishtha.richu@gmail.com>
2024-03-14 11:27:12 +0530

    firebase logIn and logOut

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ import {
   getAuth,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut
+  
 } from "firebase/auth";
 
 const firebaseConfig = {
@@ -18,21 +21,44 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
-
 console.log(auth);
 
-const authentication = document.getElementById("authentication");
+const SignUp = document.getElementById("signup");
+const LogIn = document.getElementById("login");
+const LogOut = document.getElementById("logout");
 
-authentication.addEventListener("submit", async (e) => {
+
+SignUp.addEventListener("submit", async (e) => {
   e.preventDefault();
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
-  const user = register(email, password);
-  console.log(user);
+  const confirmPassword = document.getElementById("confirmpassword").value;
+  if(password === confirmPassword){
+    const user = register(email, password);
+    console.log(user);
+    console.log("email", email);
+    console.log("password", password);
+  }
+  else{
+    console.error("Passwords do not match");
+  }
 
-  console.log("email", email);
-  console.log("password", password);
 });
+
+LogIn.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const email = document.getElementById("loginemail").value;
+  const password = document.getElementById("loginpassword").value;
+
+  const user = login(email, password);
+  console.log(user);
+})
+
+LogOut.addEventListener("click", (e) => {
+  e.preventDefault();
+  const logOutUser = logout();
+  console.log(logOutUser);
+})
 
 async function register(email, password) {
   try {
@@ -42,9 +68,51 @@ async function register(email, password) {
       password
     );
     console.log("User created: ", userCredential.user);
-
   } catch (error) {
     console.error("Error creating account: ", error.message);
     throw error;
   }
 }
+
+async function login(email, password) {
+  try{
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    console.log("User logged in: ", userCredential.user);
+  }
+  catch(error){
+    console.error("Error logging in: ", error.message);
+    throw error; 
+  }
+}
+
+async function logout(){
+  try{
+    if(auth.currentUser){
+      await signOut(auth)
+      console.log("User logged out")
+    }
+    else{
+      console.log("No signed in user")
+    }
+  }
+  catch(error){
+    console.error("Error logging out: ", error.message);
+    throw error;
+  }
+}
+
+function displayUser(){
+  const currUser = auth.currentUser
+  const currentUser = document.getElementById("currentuser");
+  currentUser.textContent = currUser.email
+}
+
+onAuthStateChanged(auth, (user) => {
+  if(user) {
+    displayUser()
+  }
+  else{
+    const currentUser = document.getElementById("currentuser");
+    currentUser.textContent = "Not signed in"
+  }
+})
