250ca9bc200b5e4e41176af04dc024f18885fd0b
Pratishtha Kumari <pratishtha.richu@gmail.com>
2024-03-14 08:54:34 +0530

    Firebase user registration

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 import { initializeApp } from "firebase/app";
-import {getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword} from "firebase/auth";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDZyOnA8LezOJ9_eVUthKsjWSR9-YHUEBQ",
@@ -8,7 +12,7 @@ const firebaseConfig = {
   storageBucket: "spendwisely-dev.appspot.com",
   messagingSenderId: "300716824308",
   appId: "1:300716824308:web:90eea52e3949ab070670a1",
-  measurementId: "G-TQXCNT7R4C"
+  measurementId: "G-TQXCNT7R4C",
 };
 
 // Initialize Firebase
@@ -17,7 +21,30 @@ const auth = getAuth();
 
 console.log(auth);
 
-const email = document.getElementById('email').value;
-const password = document.getElementById('password').value;
+const authentication = document.getElementById("authentication");
 
+authentication.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const user = register(email, password);
+  console.log(user);
 
+  console.log("email", email);
+  console.log("password", password);
+});
+
+async function register(email, password) {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    console.log("User created: ", userCredential.user);
+
+  } catch (error) {
+    console.error("Error creating account: ", error.message);
+    throw error;
+  }
+}
