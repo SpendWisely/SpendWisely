64bb72fd5c2841acbdeaef5cbf295a76c1e602e9
pushkarbanjaree <banjarepushkar.2k3@gmail.com>
2024-03-18 13:32:37 +0530

    after_signup file updated

---
+++ demo/demo.js
@@ -0,0 +1,3 @@
+document.getElementById("clickable-box").addEventListener("click", function() {
+    window.location.href = "https://www.google.com"; // Replace "https://www.example.com" with the desired URL
+});
