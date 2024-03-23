3411934e4dfe29194b8987b0b1f94a7f06812cb7
pushkarbanjaree <banjarepushkar.2k3@gmail.com>
2024-03-23 12:21:54 +0530

    table represented

--- src/dashboard.js
+++ src/dashboard.js
@@ -29,6 +29,15 @@ txnArray.sort((a, b) => {
 });
 
 //Displaying user's transactions
+// txnDetails.innerHTML = txnArray.map((txn) => {
+//     const txnType = txn.txnType === true ? "Credit" : "Debit";
+//     return `<tr>
+//                 <td>${txn.date}</td>
+//                 <td>${txnType}</td>
+//                 <td>${txn.amount}</td>
+//                 <td>${txn.category}</td>
+//             </tr></br>`
+// })
 txnDetails.innerHTML = txnArray.map((txn) => {
     const txnType = txn.txnType === true ? "Credit" : "Debit";
     return `<tr>
@@ -36,8 +45,36 @@ txnDetails.innerHTML = txnArray.map((txn) => {
                 <td>${txnType}</td>
                 <td>${txn.amount}</td>
                 <td>${txn.category}</td>
-            </tr></br>`
+            </tr>`
 })
 
 console.log("txnArray: ", txnArray);
+
+// function AddItem(date, transaction, amount, category) {
+//     let tr = document.createElement("tr");
+//     let td1 = document.createElement("td");
+//     let td2 = document.createElement("td");
+//     let td3 = document.createElement("td");
+//     let td4 = document.createElement("td");
+
+//     td1.innerHTML = txn.date;
+//     td2.innerHTML = txnType;
+//     td3.innerHTML = txn.amount;
+//     td4.innerHTML = txn.category;
+
+//     tr.appendChild(td1);
+//     tr.appendChild(td2);
+//     tr.appendChild(td3);
+//     tr.appendChild(td4);
+
+//     tbody.appendChild(tr);
+//     console.log("reached here!!");
+// }
+
+// function AddToTable(UserDetails) {
+//     tbody.innerHTML = "";
+//     UserDetails.forEach(element => {
+//         AddItem(element.name, element.roll, element.email);
+//     });
+// }
 
