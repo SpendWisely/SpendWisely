70e9283064bf29526168d73367017e0d654c01b0
Pratishtha Kumari <pratishtha.richu@gmail.com>
2024-03-27 17:54:01 +0530

    build configs

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,29 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
 export default defineConfig({
     server: {
       watch: {
         usePolling: true
+      }
+    },
+
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          login: resolve(__dirname, 'src/login.html'),
+          signup: resolve(__dirname, 'src/signup.html'),
+          aftersignup: resolve(__dirname, 'src/after_signup.html'),
+          dashboard: resolve(__dirname, 'src/dashboard.html'),
+          transaction: resolve(__dirname, 'src/transaction.html'),
+        }
+      }
+    },
+
+    esbuild: {
+      supported: {
+        'top-level-await': true
       }
     }
   })
