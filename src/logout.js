eb2c01d26d209934f2218c85712de539fe18efa3
Pratishtha Kumari <pratishtha.richu@gmail.com>
2024-03-29 16:05:18 +0530

    logout firebase config fix

--- src/logout.js
+++ src/logout.js
@@ -1,32 +1,21 @@
-import { initializeApp } from "firebase/app";
 import {
   getAuth,
-  // signInWithEmailAndPassword,
-  onAuthStateChanged,
   signOut,
 } from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDZyOnA8LezOJ9_eVUthKsjWSR9-YHUEBQ",
-  authDomain: "spendwisely-dev.firebaseapp.com",
-  projectId: "spendwisely-dev",
-  storageBucket: "spendwisely-dev.appspot.com",
-  messagingSenderId: "300716824308",
-  appId: "1:300716824308:web:90eea52e3949ab070670a1",
-  measurementId: "G-TQXCNT7R4C",
-};
+import {app} from "../firebase";
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
+const auth = getAuth(app);
 console.log(auth);
+console.log("currUser",auth.currentUser);
+
 
 const LogOut = document.getElementById("logout");
 
 LogOut.addEventListener("click", (e) => {
   e.preventDefault();
   const logOutUser = logout();
-  console.log(logOutUser);
+  console.log("logout: ", logOutUser)
 });
 
 async function logout() {
